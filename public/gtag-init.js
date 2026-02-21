2fd496f0e3c45b1ab0e68014bec3dae1f1d593ff
Terence <hterence985@gmail.com>
2026-02-21 23:15:00 +0530

    fix: update Content Security Policy and move Google Analytics initialization to external script

---
+++ public/gtag-init.js
@@ -0,0 +1,6 @@
+// gtag initialization moved out of inline HTML to satisfy strict CSP
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);} 
+gtag('js', new Date());
+// Replace the ID with your GA measurement ID if different
+gtag('config', 'G-LVXW2S64FT');
