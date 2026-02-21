c2810aba11560c15d3a951fab693679118e9be1a
Terence <hterence985@gmail.com>
2026-02-22 00:04:10 +0530

    fix: update YouTube link in SOCIALS constant for accurate redirection

--- src/constants/socials.js
+++ src/constants/socials.js
@@ -3,5 +3,5 @@ import { Instagram, Facebook, Youtube } from "lucide-react";
 export const SOCIALS = [
   { icon: Instagram, href: "https://www.instagram.com/mrevents.co.in/", label: "Instagram" },
   { icon: Facebook, href: "#", label: "Facebook" },
-  { icon: Youtube, href: "#", label: "YouTube" },
+  { icon: Youtube, href: "https://www.youtube.com/@MREvents-l2f", label: "YouTube" },
 ];
